e90ea67ddb6253e9b85a73ace2a95b1deee807e7
maker3391 <cjstn3391@gmail.com>
2026-05-12 10:58:16 +0900

    초기 백엔드 프로젝트 구조 및 설정 추가

---
+++ backend/src/test/java/com/golladrim/GolladrimApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.golladrim;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GolladrimApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
